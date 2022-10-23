b7ee2652759b48bc3947a34e72338edc5647130c
Pierre Quereuil <pierrequereuil@gmail.com>
2022-10-22 17:42:12 -0700

    Refreshed Django Project

--- artstudio/artstudio/wsgi.py
+++ artstudio/artstudio/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for sadface project.
+WSGI config for artstudio project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sadface.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'artstudio.settings')
 
 application = get_wsgi_application()
