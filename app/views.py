a0b21b953b16ddca155b2a83d5ece7039cba4c2f
coder-creater <55065811+coder-creater@users.noreply.github.com>
2022-11-01 21:52:55 -0700

    changing function for nixpacks to hopefully work

--- app/views.py
+++ app/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
 from json import JSONDecoder
+from django.views import View
 from django.http import JsonResponse
 
 def test(request):
   return JsonResponse({"message": "Hello World"})
+
+# def index(request):
+#     return render(request, 'app/templates/index.html')
+
+def index(request, template="index.html"):
+    return render(request, template, {})
 
 # def generate(request):
